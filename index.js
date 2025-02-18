76709ccd92bbec586afee971e841d9b3c13ef126
sonjuhyeon <ths120521113105@daum.net>
2025-02-18 20:53:47 +0900

    초기세팅

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const PORT = 8080;
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (request, response) => {
+  try {
+    response.send(
+      "Hello World"
+    );
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
